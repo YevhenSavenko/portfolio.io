1bc1d55b6b30eb231253858ee41e72badf4f37ab
Yevhen Savenko <eugene.savenko@regulus.team>
2020-12-18 20:57:01 +0200

    #3 Fix first screen for mobile

--- project/src/js/script.js
+++ project/src/js/script.js
@@ -112,3 +112,26 @@ for (let i = 0; i < linkNav.length; i++) {
         }
     }, false);
 }
+
+(function(d){
+    function c(k){return(d.cookie.match('(^|; )'+k+'=([^;]*)')||0)[2];}
+    var ua = navigator.userAgent,
+        ismobile = / mobile/i.test(ua),
+        mgecko = !!( / gecko/i.test(ua) && / firefox\//i.test(ua)),
+        wasmobile = c('wasmobile') === "was",
+        desktopvp = 'user-scalable=yes, width=1600, minimum-width=1600',
+        el;
+    if (ismobile && !wasmobile) {
+        d.cookie = "wasmobile=was";
+    }
+    else if (!ismobile && wasmobile) {
+        if (mgecko) {
+            el = d.createElement('meta');
+            el.setAttribute('content', desktopvp);
+            el.setAttribute('name', 'viewport');
+            d.getElementsByTagName('head')[0].appendChild(el);
+        }else{
+            d.getElementsByName('viewport')[0].setAttribute('content', desktopvp);
+        }
+    }
+}(document));
